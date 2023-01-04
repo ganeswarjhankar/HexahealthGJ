606df6e6c7af33e62cd72f03ff65f914dc0823b2
ganeswar <ganeswarjhankar@gmail.com>
2023-01-04 10:39:16 +0530

    4th jan 2023

---
+++ DisplayMarketingHexahealth.py
@@ -0,0 +1,52 @@
+import time
+import requests
+#import response as response
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+
+
+
+S = Service("D:\\chromedriver.exe")
+driver = webdriver.Chrome(service=S)
+driver.maximize_window()
+url = driver.get("https://www.hexahealth.com/marketing/display/laser-fissure-bangalore")
+
+
+# Locate the button element
+#buttons = driver.find_elements(By.XPATH,"//*[@id='whtsapHeaderBtn']/span/b")
+buttons = driver.find_element(By.XPATH,"//*[@id='whtsapHeaderBtn']/span/b").click()
+#buttons = driver.find_elements(By.CSS_SELECTOR,'WhatsApp Expert')
+
+
+
+
+
+    # code to execute on each page goes here
+
+    # Get the current URL
+current_url = driver.current_url
+print(current_url)
+# Verify that the current URL contains the expected value
+response = requests.get(current_url)
+if 'api.' in current_url:
+    if response.status_code == 200:
+        print("Status Code 200 Ok")
+    else:
+        print("Failed")
+
+    print('Verification successful')
+
+else:
+    print('Verification failed as the URl Not containing "api"')
+
+#response = requests.get(current_url)
+
+time.sleep(3)
+#driver.back()
+
+
+buttons = driver.find_elements(By.XPATH,"//*[@id='whtsapHeaderBtn']/span/b")
+
--- Roughcode2.py
+++ Roughcode2.py
@@ -1,41 +1,52 @@
-
-
-
 import time
-
 import requests
+import response as response
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+#from selenium.webdriver.support.ui import WebDriverWait
+#from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 
-#from MarketingCustomizedHexahealth import response
+
 
 S = Service("D:\\chromedriver.exe")
 driver = webdriver.Chrome(service=S)
 driver.maximize_window()
 url = driver.get("https://www.hexahealth.com/marketing/display/laser-fissure-bangalore")
 
+
 # Locate the button element
-#button = driver.find_element(By.XPATH,"//*[@id='whtsapHeaderBtn']/span/b").click()
-buttons = driver.find_elements(By.CSS_SELECTOR,'WhatsApp Expert')
-print(buttons)
-time.sleep(5)
+#buttons = driver.find_elements(By.XPATH,"//*[@id='whtsapHeaderBtn']/span/b")
+buttons = driver.find_element(By.XPATH,"//*[@id='whtsapHeaderBtn']/span/b").click()
+#buttons = driver.find_elements(By.CSS_SELECTOR,'WhatsApp Expert')
 
 
 
 
-# Click each button
-for button in buttons:
-    button.click()
-#print(buttons)
-# Close the browser
-#driver.quit()
+
+    # code to execute on each page goes here
+
+    # Get the current URL
+current_url = driver.current_url
+print(current_url)
+# Verify that the current URL contains the expected value
+response = requests.get(current_url)
+if 'api.' in current_url:
+    if response.status_code == 200:
+        print("Status Code 200 Ok")
+    else:
+        print("Failed")
+
+    print('Verification successful')
+
+else:
+    print('Verification failed as the URl Not containing "api"')
+
+#response = requests.get(current_url)
+
+time.sleep(3)
+#driver.back()
 
 
-number_li_elems=len(WebDriverWait(browser,30).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "li.clickable_area"))))
-for x in range(number_li_elems):
-    # you have to get the element by index every time, otherwise you will get StaleElement Exception
-    button = browser.find_element_by_css_selector("li.clickable_area:nth-child(" + str(x+1) + ") > div:nth-child(3)")
-    button.click()
+buttons = driver.find_elements(By.XPATH,"//*[@id='whtsapHeaderBtn']/span/b")
+
